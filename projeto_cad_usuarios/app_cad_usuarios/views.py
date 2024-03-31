f4b6af6bb23459c6d60143bc65b23c71be4b0058
João Nogues Neto <1828777@aluno.univesp.br>
2024-03-30 22:17:50 -0300

    testes para futura pagina incial

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import usuario
 
-# Create your views here.
+def home(request):
+    return render(request,'usuarios/home.html')
+
+def usuarios(request):
+    novo_usuario = usuario()
+    novo_usuario.nome = request.POST.get("nome")
+    novo_usuario.rs = request.POST.get("rs")
+    novo_usuario.save()
+
+    usuarios = {
+        'usuarios': usuario.objects.all()
+    }
+
+    return render(request,"usuarios/usuarios.html",usuarios)
+
+    
