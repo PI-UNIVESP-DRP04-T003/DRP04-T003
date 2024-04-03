c971b8098880994b2684adfc6a916fa9f0a1a030
João Nogues Neto <1828777@aluno.univesp.br>
2024-04-02 21:19:36 -0300

    atualizado mas ainda não funcionando... vou dar um jeito...

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,8 +1,29 @@
+# Inicio Login na página home e link para as páginas pagina1 e pagina 2!
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+
+def home(requests):
+    return render(requests, 'core/home.html')
+
+
+@login_required
+def pagina1(requests):
+    return render(requests, 'core/pagina1.html')
+
+
+@login_required
+def pagina2(requests):
+    return render(requests, 'core/pagina2.html')
+
+# Inicio parte cadastro !
+
 from django.shortcuts import render
 from .models import usuario
 
 def home(request):
-    return render(request,'usuarios/home.html')
+    return render(request,'usuarios/cadastro.html')
 
 def usuarios(request):
     novo_usuario = usuario()
--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -1,4 +1,25 @@
 
+#Parte Login
+
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+from projeto_cad_usuarios import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('pagina1/', views.pagina1, name='pagina1'),
+    path('pagina2/', views.pagina2, name='pagina2'),
+    path('accounts/', include('django.contrib.auth.urls'))
+]
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
+
+
+#Parte Cadastro
 from django.urls import path
 from app_cad_usuarios import views
 
